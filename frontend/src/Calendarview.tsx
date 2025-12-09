0a56c10017a710954e5daf75a1d07e0bc1044c06
Biniam Gashaw <biniamg84@yahoo.com>
2025-12-09 01:13:16 -0800

    Revert "added day view"
    This reverts commit a1c6754640f4294dbb324e281521897069867fc7.

--- frontend/src/Calendarview.tsx
+++ frontend/src/Calendarview.tsx
@@ -1,36 +1,28 @@
-import React, { useState } from "react";
+//import React from "react";
+import React, {useState} from "react";
+import app from "./App";
+import {NavLink} from "react-router-dom";
+import Sidebar from "./Sidebar";
 import "./calendar.css";
 
 const CalendarView = () => {
-  const [calendarView, setCalendarView] = useState<"month" | "week" | "day">(
-    "month",
-  );
-  const hours = Array.from({ length: 12 }, (_, i) => i + 8);
+    // state allows month, week, day view, efaults to month
+    const [calendarView, setCalendarView] = useState<"month" | "week" | "day">("month");
 
-  return (
-    <div className="calendarContainer">
-      <div className="calendarView">
-        <div className="calendarButtons">
-          <button onClick={() => setCalendarView("month")}>Month</button>
-          <button onClick={() => setCalendarView("week")}>Week</button>
-          <button onClick={() => setCalendarView("day")}>Day</button>
-        </div>
-        <div className="calendarViewDisplay">
-          {calendarView === "month" && <div>Month View</div>}
-          {calendarView === "week" && <div>Week View</div>}
-          {calendarView === "day" && (
-            <div>
-              {hours.map((hour) => (
-                <div key={hour} className="timeSlot">
-                  {hour > 12 ? hour - 12 : hour}:00 {hour >= 12 ? "PM" : "AM"}
-                </div>
-              ))}
+    return (
+        <div className="calendarView">
+            <div className="calendarButtons">
+                <button onClick={()=>setCalendarView("month")}>Month</button>
+                <button onClick={()=>setCalendarView("week")}>Week</button>
+                <button onClick={()=>setCalendarView("day")}>Day</button>
             </div>
-          )}
+            <div className="calendarViewDisplay">
+                {calendarView === "month" && <div>Month View</div>}
+                {calendarView === "week" && <div>Week View</div>}
+                {calendarView === "day" && <div>Day View</div>}
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default CalendarView;
