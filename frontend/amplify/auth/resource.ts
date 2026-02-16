1af9ba9f4cdc044fe1537385e8e15f91c8f30a17
Ankush Joshi <58573571+aannkooss@users.noreply.github.com>
2026-02-15 23:40:28 -0800

    Add Amazon login and clean up URLs
    Added Amazon login configuration and removed main domain URLs from callback and logout URLs.

--- frontend/amplify/auth/resource.ts
+++ frontend/amplify/auth/resource.ts
@@ -14,25 +14,31 @@ export const auth = defineAuth({
         clientSecret: secret('GOOGLE_CLIENT_SECRET'),
         scopes: ['email', 'profile', 'openid'],
       },
+
+      loginWithAmazon: {
+        clientId: secret('AMAZON_CLIENT_ID'),
+        clientSecret: secret('AMAZON_CLIENT_SECRET'),
+        scopes: ['profile']
+      }, 
       callbackUrls: [
         'http://localhost:3000/', 
+        // Amplify Console domain for auth-start-branch. DELETE THESE WHEN BRANCH IS DELETED
+        'https://feature-auth-fresh-start.d2i3jqbsdy4snq.amplifyapp.com/',
+        'https://feature-auth-fresh-start.d2i3jqbsdy4snq.amplifyapp.com',
         // Dev Amplify Console domain
         'https://dev.d2i3jqbsdy4snq.amplifyapp.com/',
         // Dashboard domain 
         'https://dashboard.d2i3jqbsdy4snq.amplifyapp.com/',
-        // Main domain
-        'https://main.d2i3jqbsdy4snq.amplifyapp.com/',
-        'https://main.d2i3jqbsdy4snq.amplifyapp.com'
       ],
       logoutUrls: [
         'http://localhost:3000/',
+         // Amplify Console domain for auth-start-branch. DELETE THESE WHEN BRANCH IS DELETED
+        'https://feature-auth-fresh-start.d2i3jqbsdy4snq.amplifyapp.com/',
+        'https://feature-auth-fresh-start.d2i3jqbsdy4snq.amplifyapp.com',
         // Dev Amplify Console domain
         'https://dev.d2i3jqbsdy4snq.amplifyapp.com/',
         // Dashboard domain
-        'https://dashboard.d2i3jqbsdy4snq.amplifyapp.com/',
-        // Main Domain
-        'https://main.d2i3jqbsdy4snq.amplifyapp.com/',
-        'https://main.d2i3jqbsdy4snq.amplifyapp.com'
+        'https://dashboard.d2i3jqbsdy4snq.amplifyapp.com/'
       ],
     },
   },
