b14ff3da2dd82541ca856b6d2091fc8adb7471df
Biniam Gashaw <biniamg84@yahoo.com>
2026-04-14 20:24:40 -0700

    Revert "Merge branch 'darkMode' into feature/Google-calendar-sync"
    This reverts commit 9e55cd4e4e4fcbdcba8a1c07e4b758370740b473, reversing
    changes made to 08e8fc948c614c513ef689090ce8a7bd674fc330.

--- frontend/src/pages/ProfileContainer/profilecontainer.tsx
+++ frontend/src/pages/ProfileContainer/profilecontainer.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
-import Profile from "../Profile/Profile";
-import "../../styles/SharedLayout.css";
+import CalendarView from "../Calendar/CalendarView";
 
-const ProfileContainer = () => {
+
+import Profile from "../Profile/Profile";
+
+
+const  ProfileContainer = () => {
   return (
-    <div className="app-wrapper">
+    <div>
       <div className="dashboardHeader">
         <h1>AgendaAI</h1>
         <h2>Profile</h2>
       </div>
     
-      {/* we Used these groups from the css file in order to organize the dashboard into 3 main panels, left middle and right */}
+      {/* we Used these groups from the css file in order to organize the dashboardinto 3 main pannels, left middle and right*/}
       <div className="dashboardGroup">
         <Sidebar />
 
