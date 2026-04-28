d75cc1dd005b29e1f0aaeccbd59af102269a8145
itsalexmedal <cas9400@gmail.com>
2026-04-27 21:48:35 -0700

    comments

--- frontend/src/pages/Calendar/CalendarView.tsx
+++ frontend/src/pages/Calendar/CalendarView.tsx
@@ -5,7 +5,7 @@ import MonthView from "./MonthView";
 import WeekCalendar from "./WeekView";
 import DayView from "./DayView";
 import BackButton from "../../components/BackButton";
-
+//alex, container to switch views 
 const CalendarView = () => {
     const location = useLocation();
 
